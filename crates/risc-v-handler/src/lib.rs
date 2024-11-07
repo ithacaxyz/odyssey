6bdd22a6a7ce99b9a763727628bbb7308eaee47d
0xurb <urbadeil@gmail.com>
2024-11-07 23:04:05 +0000

    feat: declared handler

--- crates/risc-v-handler/src/lib.rs
+++ crates/risc-v-handler/src/lib.rs
@@ -1,1 +1,5 @@
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
+use reth_revm::{handler::register::EvmHandler, Database};
+
+pub fn risc_v_handle_register<EXT, DB: Database>(handler: &mut EvmHandler<'_, EXT, DB>) {}
